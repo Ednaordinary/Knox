f009189c0110e333b6c4c28798a3acbaff4d9e7c
Edna <88869424+Ednaordinary@users.noreply.github.com>
2025-07-22 23:55:12 -0600

    change around slice settings

--- slicer.py
+++ slicer.py
@@ -23,4 +23,4 @@ class Slicer:
     def slice(self, paths, out):
         # yes this is gross
         # doing it anyways
-        subprocess.run("./OrcaSlicer.AppImage --load-settings \"orca-input/a1.json;orca-input/standard.json\" --load-filaments \"orca-input/pla.json\" -arrange 2 --orient 2 --slice 0 --outputdir " + str(out) + " --slice 0 " + " ".join(paths), shell=True, cwd=os.getcwd())
+        subprocess.run("./OrcaSlicer.AppImage --load-settings \"orca-input/standard.json;orca-input/a1.json\" --load-filaments \"orca-input/pla.json\" --orient 1 --arrange 1 --slice 0 --outputdir " + str(out) + " --slice 0 " + " ".join(paths), shell=True, cwd=os.getcwd())
