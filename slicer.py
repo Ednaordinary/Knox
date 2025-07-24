9686c1d6dc1d3b2a2f1ff37c9168dcad42c196f5
Edna <88869424+Ednaordinary@users.noreply.github.com>
2025-07-24 05:54:58 -0600

    add outputting with supports

--- slicer.py
+++ slicer.py
@@ -20,7 +20,10 @@ class Slicer:
         time = time_line.split(":")[2].strip()
         print(filament, time)
         return (filament, time)
-    def slice(self, paths, out):
+    def slice(self, paths, out, support=False):
         # yes this is gross
         # doing it anyways
-        subprocess.run("./OrcaSlicer.AppImage --load-settings \"orca-input/standard.json;orca-input/a1.json\" --load-filaments \"orca-input/pla.json\" --orient 1 --arrange 1 --slice 0 --outputdir " + str(out) + " --slice 0 " + " ".join(paths), shell=True, cwd=os.getcwd())
+        if support:
+            subprocess.run("./OrcaSlicer.AppImage --load-settings \"orca-input/standard-support.json;orca-input/a1.json\" --load-filaments \"orca-input/pla.json\" --arrange 1 --orient 1 --arrange 1 --slice 0 --outputdir " + str(out) + " " + " ".join(paths), shell=True, cwd=os.getcwd())
+        else:
+            subprocess.run("./OrcaSlicer.AppImage --load-settings \"orca-input/standard.json;orca-input/a1.json\" --load-filaments \"orca-input/pla.json\" --arrange 1 --orient 1 --arrange 1 --slice 0 --outputdir " + str(out) + " " + " ".join(paths), shell=True, cwd=os.getcwd())
